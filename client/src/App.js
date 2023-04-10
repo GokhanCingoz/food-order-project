67735775c594de7ed93014b7e96f99aa49eab8d1
GokhanCingoz <gokhancingoz1@gmail.com>
2023-04-10 15:38:28 +0300

    Redux, Payment Method, Admin Panel, Order Pages Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
+import { Routes, Route } from "react-router-dom";
+import CartPage from "./pages/CartPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import OrdersPage from "./pages/OrdersPage";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sepet" element={<CartPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/myorders" element={<OrdersPage />} />
+        <Route path="/admin/*" element={<AdminPage />} />
+      </Routes>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -4,11 +4,17 @@ const db = require("./db");
 const burgerModel = require("./models/burgerModel");
 const app = express();
 const burgersRoute = require("./routes/burgersRoute");
+const usersRoute = require("./routes/usersRoute");
+const ordersRoute = require("./routes/ordersRoute");
 
 app.use(express.json());
 app.use(cors());
 
+//routing blokları
+//http://localhost:4000/api/burgers/getBurgers
 app.use("/api/burgers/", burgersRoute);
+app.use("/api/users/", usersRoute);
+app.use("/api/orders/", ordersRoute);
 
 //serverımızı inşa edeceğimiz portu belirleyerek ayağa kaldırdık.
 app.listen(4000, () => {
